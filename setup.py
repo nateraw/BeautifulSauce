8dc1921f35434c5dbf0b8b31cefd9635bbb69641
nateraw <nxr9266@g.rit.edu>
2019-09-09 20:32:45 -0600

    Descriptions read from __doc__ now and I updated PyPi version

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
+"""BeautifulSoup's saucy sibling!
+
+BeautifulSauce provides:
+
+- ability to featurize HTML documents with custom functions
+- integration of CSS attributes (with inheritance!) applied to each Tag
+- options for indexing HTML with numerical index lists
+- built in utility functions that cover common boilerplate snippets
+"""
+
+DOCLINES = (__doc__ or '').split("\n")
+
 from setuptools import setup, find_packages
 
 with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
 
-LONG_DESC = open("README.md").read()
-
 setup(
     name="BeautifulSauce",
-    version='0.0.1',
+    version='0.0.7',
     author="Nathan Raw",
     author_email="naterawdata@gmail.com",
-    description="Tools that make BeautifulSoup more beautiful.",
-    long_description=LONG_DESC,
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
     license="MIT",
     url="https://github.com/nateraw/BeautifulSauce",
     install_requires=requirements,
